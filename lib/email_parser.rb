03e574bb7f8c6f0dbf3f0f37fa82785f59b46719
olusola olagoroye <solaolagoroye@gmail.com>
2019-12-19 09:39:28 +0000

    Done.

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -2,3 +2,26 @@
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
+class EmailAddressParser
+  attr_accessor :cvs_emails
+  def initialize(cvs_email)
+    @cvs_emails = cvs_email
+  end
+  
+  def parse
+     cvs_emails.delete(',').split.uniq
+  
+  # @cvs_email = cvs_email.gsub(/[\s]|,/)
+  # cvs_email.uniq
+  end
+
+end
+  
+  
+  
+  
+  
+  
+#   gsub => Removing invalid characters (by making the 2nd argument an empty string)
+# Replacing placeholders & acronyms by their full values
+# Using patterns & logic to change a string
